27011eb2cd45db4663445161384d72ed9a99c142
hyunsunny3 <hs9473@naver.com>
2023-08-31 14:57:35 +0900

    Dice Study

---
+++ src/dice/Index.js
@@ -0,0 +1,4 @@
+import ReactDOM from 'react-dom';
+import SunnyDice from './SunnyDice';
+
+ReactDOM.render(<SunnyDice />, document.getElementById('root'));
--- src/dice/SunnyDice.js
+++ src/dice/SunnyDice.js
@@ -1,38 +1,77 @@
-import React from "react";
+import { useState } from 'react';
+import Button from './Button';
+import HandButton from './HandButton';
+import HandIcon from './HandIcon';
+import { compareHand, generateRandomHand } from './Utils';
 
-const SunnyDice = () => {
-    
-    const WINS = {
-        rock: 'scissor',
-        scissor: 'paper',
-        paper: 'rock',
-    };
-    
-    function getResult(left, right) {
-        if (WINS[left] === right) return '승리';
-        else if (left === WINS[right]) return '패배';
-        return '무승부';
-    }
-    
-    function handleClick() {
-        console.log('가위바위보!');
-    }
-    
-    const me = 'rock';
-    const other = 'scissor';
-    
-    return (
-      <>
-        <div id="wrap">
-            주사위 Game~
-            <h1 id="title">가위바위보</h1>
-            <h2>{getResult(me, other)}</h2>
-            <button onClick={handleClick}>가위</button>
-            <button onClick={handleClick}>바위</button>
-            <button onClick={handleClick}>보</button>
-        </div>
-      </>
-    );
+const INITIAL_VALUE = 'rock';
+
+function getResult(me, other) {
+  const comparison = compareHand(me, other);
+  if (comparison > 0) return '승리';
+  if (comparison < 0) return '패배';
+  return '무승부';
+}
+
+function SunnyDice() {
+  // hand와 otherHand를 state로 바꿔 주세요
+  const [hand, setHand] = useState(INITIAL_VALUE);
+  const [otherHand, setOtherHand] = useState(INITIAL_VALUE);
+  const [gameHistory, setGameHistory] = useState([]);
+  const [score, setScore] = useState(0);
+  const [otherScore, setOtherScore] = useState(0);
+  const [bet, setBet] = useState(1);
+
+  const handleButtonClick = (nextHand) => {
+    const nextOtherHand = generateRandomHand();
+    const nextHistoryItem = getResult(nextHand, nextOtherHand);
+    const comparison = compareHand(nextHand, nextOtherHand);
+    setHand(nextHand);
+    setOtherHand(nextOtherHand);
+    setGameHistory([...gameHistory, nextHistoryItem]);
+    if (comparison > 0) setScore(score + bet);
+    if (comparison < 0) setOtherScore(otherScore + bet);
   };
 
-  export default SunnyDice;
+  const handleClearClick = () => {
+    setHand(INITIAL_VALUE);
+    setOtherHand(INITIAL_VALUE);
+    setGameHistory([]);
+    setScore(0);
+    setOtherScore(0);
+    setBet(1);
+  };
+
+  const handleBetChange = (e) => {
+    let num = Number(e.target.value);
+    if (num > 9) num %= 10;
+    if (num < 1) num = 1;
+    num = Math.floor(num);
+    setBet(num);
+  };
+
+  return (
+    <div>
+      <Button onClick={handleClearClick}>처음부터</Button>
+      <div>
+        {score} : {otherScore}
+      </div>
+      <div>
+        <HandIcon value={hand} />
+        VS
+        <HandIcon value={otherHand} />
+      </div>
+      <div>
+        <input type="number" value={bet} min={1} max={9} onChange={handleBetChange}></input>
+      </div>
+      <p>승부 기록: {gameHistory.join(', ')}</p>
+      <div>
+        <HandButton value="rock" onClick={handleButtonClick} />
+        <HandButton value="scissor" onClick={handleButtonClick} />
+        <HandButton value="paper" onClick={handleButtonClick} />
+      </div>
+    </div>
+  );
+}
+
+export default SunnyDice;
